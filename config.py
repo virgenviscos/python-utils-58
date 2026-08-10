6ed0fd8a0ffdd83d4a6f085ffff2b6d569f85dfe
Virgen Viscos <virgenviscos@inbox.lv>
2026-08-10 11:09:14 +0300

    add configuration loader with defaults

--- config.py
+++ config.py
@@ -2,24 +2,35 @@ import json
 import os
 
 class ConfigLoader:
-    def __init__(self, default_config_file):
-        self.default_config_file = default_config_file
-        self.config = self.load_defaults()
+    def __init__(self, default_config, user_config_path='config.json'):
+        self.default_config = default_config
+        self.user_config_path = user_config_path
+        self.config = self.load_config()
 
-    def load_defaults(self):
-        with open(self.default_config_file, 'r') as file:
-            return json.load(file)
-
-    def load_user_config(self, user_config_file):
-        if os.path.exists(user_config_file):
-            with open(user_config_file, 'r') as file:
-                user_config = json.load(file)
-            self.config.update(user_config)
+    def load_config(self):
+        config = self.default_config.copy()
+        if os.path.exists(self.user_config_path):
+            with open(self.user_config_path, 'r') as user_config_file:
+                user_config = json.load(user_config_file)
+                config.update(user_config)
+        return config
 
     def get(self, key, default=None):
         return self.config.get(key, default)
 
-if __name__ == '__main__':
-    loader = ConfigLoader('default_config.json')
-    loader.load_user_config('user_config.json')
-    print(loader.get('some_key', 'default_value'))
+    def set(self, key, value):
+        self.config[key] = value
+        with open(self.user_config_path, 'w') as user_config_file:
+            json.dump(self.config, user_config_file)
+
+# Example default configuration
+DEFAULT_CONFIG = {
+    'setting1': 'value1',
+    'setting2': 'value2',
+    'setting3': 10
+}
+
+# Usage:
+# config_loader = ConfigLoader(DEFAULT_CONFIG)
+# print(config_loader.get('setting1'))
+# config_loader.set('setting4', 'value4')
